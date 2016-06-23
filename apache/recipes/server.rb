8bb27ed67f6069d9c187bc6444a1b69f0098f6af
ChefDK User <chef@ip-10-159-135-242.ec2.internal>
2016-06-23 14:09:59 +0000

    New template for index.html

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -2,13 +2,8 @@ package 'httpd' do
 	action :install
 end
 
-file '/var/www/html/index.html' do
-	content "<H1>Hello World!</H1>
-<H2>HOSTNAME: #{node['hostname']}</H2>
-<H2>IP: #{node['ipaddress']}</H2>
-<H2>MEM: #{node['memory']['total']}</H2>
-<H2>CPU: #{node['cpu']['0']['mhz']} MHz</H2>
-"
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
 end
 
 service 'httpd' do

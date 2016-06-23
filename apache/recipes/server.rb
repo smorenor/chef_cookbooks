6fe6df4dd9d0cc4e392a3c1ed4b6ecd8b772897b
ChefDK User <chef@ip-10-159-135-242.ec2.internal>
2016-06-23 13:30:39 +0000

    Cambios en apache

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -1,8 +1,14 @@
 package 'httpd' do
 	action :install
 end
+
 file '/var/www/html/index.html' do
-	content '<h1>Hello World!</h1>'
+	content "<H1>Hello World!</H1>
+<H2>HOSTNAME: #{node['hostname']}</H2>
+<H2>IP: #{node['ipaddress']}</H2>
+<H2>MEM: #{node['memory']['total']}</H2>
+<H2>CPU: #{node['cpu']['0']['mhz']} MHz</H2>
+"
 end
 
 service 'httpd' do
